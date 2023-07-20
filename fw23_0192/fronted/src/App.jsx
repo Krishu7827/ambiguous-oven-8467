228afc0975d80d9c95298430fa42b9d62f8e6de5
Andy18j <115467259+Andy18j@users.noreply.github.com>
2023-07-20 22:49:03 +0530

    day-2

---
+++ fw23_0192/fronted/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import SignInn from './Components/SignInn';
+import Signup from './Components/Signup';
+
+
+function App() {
+  return (
+    <div className="App">
+     <SignInn/>/
+    {/* <Signup/> */}
+    </div>
+  );
+}
+
+export default App;
