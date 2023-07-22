5c473e14a3760e5eae51f62633642cc49a0dcef4
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2023-07-22 20:01:59 +0530

    course backend done

--- Backend/SignIn_SignUp_server/index.js
+++ Backend/SignIn_SignUp_server/index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const {connection}=require("./Connections/db")
 const {userRouter}=require("./Routes/user.routes")
+const {courseRoute}=require("./Routes/courses.routes")
 
 require("dotenv").config()
 const cors = require('cors')
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/users",userRouter)
+app.use("/course",courseRoute)
 
 
 
