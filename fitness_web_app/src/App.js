a0b12bf6622c2530898efb362e94a4f11fe1c1db
tanuj8851 <tanujmaurya40@gmail.com>
2023-07-23 17:26:04 +0530

    NAV.jsx Done

--- fitness_web_app/src/App.js
+++ fitness_web_app/src/App.js
@@ -1,21 +1,21 @@
-import UncontrolledExample from './Components/Home';
+// import UncontrolledExample from './Components/Home';
 import './App.css';
 import CourseOfHome from './Components/CourseOfHome';
 import OurInstructor from './Components/OurInstructor';
 import AboutUs from './Components/AboutUs';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Testimonial from './Components/testimonial';
 import SocialMedia from './Components/SocialMedia';
 function App() {
   return (
- <div id='App'>
-   <UncontrolledExample/>
-   <CourseOfHome/>
-   <OurInstructor/>
-   <Testimonial/>
-   <AboutUs/>
-   <SocialMedia/>
- </div>
+    <div id='App'>
+      <UncontrolledExample />
+      <CourseOfHome />
+      <OurInstructor />
+      <Testimonial />
+      <AboutUs />
+      <SocialMedia />
+    </div>
   );
 }
 
---
+++ fitness_web_app/src/Components/Nav.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+
+const nav = () => {
+
+	return (
+		<nav className="navbar">
+			<div className="mid-nav">
+				<div className="nav-logo">
+					<a href="#">
+						<h1>Joesthetics Club</h1>
+					</a>
+				</div>
+				<div className="nav-list">
+					<ul>
+						<li>
+							<a href="#">HOME</a>
+						</li>
+						<li>
+							<a href="">ABOUT</a>
+						</li>
+						<li>
+							<a href="">FIND THE GYM</a>
+						</li>
+						<li>
+							<a href="">Classes</a>
+						</li>
+						<li>
+							<a href="">Membership</a>
+						</li>
+						<li>
+							<a href="">Franchise</a>
+						</li>
+						<li>
+							<a href="">Faq</a>
+						</li>
+						<li>
+							<a href="">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+	)
+}
+
+export default nav;
